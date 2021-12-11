49cc4eda8f9d77ed6dcef7fda3ebf89eb85588ae
gowtham <mohangowthamraj@gmail.com>
2021-12-11 18:56:43 +0530

    Initial project setup

---
+++ src/test/java/io/smacs/brewery/BreweryApplicationTests.java
@@ -0,0 +1,13 @@
+package io.smacs.brewery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BreweryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
